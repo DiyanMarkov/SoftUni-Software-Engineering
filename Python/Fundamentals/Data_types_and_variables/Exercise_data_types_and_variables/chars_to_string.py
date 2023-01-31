ce81d18106aa37d776987a9489d9aff3c2c4e715
DiyanMarkov <dian.markov@abv.bg>
2023-01-31 20:03:28 +0200

    wrong files

--- Python/Fundamentals/Data_types_and_variables/Exercise_data_types_and_variables/chars_to_string.py
+++
@@ -1,4 +0,0 @@
-first_character = input()
-second_character = input()
-third_character = input()
-print(f'{first_character}{second_character}{third_character}')
--- Python/Fundamentals/Data_types_and_variables/Exercise_data_types_and_variables/print_part_of_the_ascii_table.py
+++
@@ -1,8 +0,0 @@
-starting_character = int(input())
-ending_character = int(input())
-current_character = 0
-
-for character in range(starting_character, ending_character + 1):
-    current_character += character
-    print(chr(current_character), end=' ')
-    current_character = 0
